3fe2dfd165911295f8b8d7a700bf647a6bf543bf
paper-daemon <juriojuriojurio6@gmail.com>
2026-08-30 12:50:50 +0900

    Add Shop Image QA input boundary regression test

---
+++ tests/test_app.js
@@ -0,0 +1,19 @@
+const assert=require('node:assert/strict');
+
+global.location={href:'https://paper-daemon.github.io/shop-image-qa/'};
+global.document={querySelector:()=>({addEventListener(){},innerHTML:''})};
+
+const {escHtml,safeOfferUrl,imageNotes}=require('../app.js');
+
+assert.equal(
+  escHtml('<img src=x onerror=alert(1)>.png'),
+  '&lt;img src=x onerror=alert(1)&gt;.png'
+);
+assert.equal(safeOfferUrl('javascript:alert(1)'),'');
+assert.equal(safeOfferUrl('https://example.com/x'),'https://example.com/x');
+assert.deepEqual(
+  imageNotes(800,1200,6*1024*1024,'image/gif'),
+  ['解像度が小さめ','容量大きめ','一般的なWeb形式ではない']
+);
+
+console.log('4 assertions PASS');
